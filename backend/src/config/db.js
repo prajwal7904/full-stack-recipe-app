3df1004870ac21bbdef501e87b1f6ac05355091d
preethesh <preethesh4263@gmail.com>
2025-12-29 12:14:45 +0530

    fix: resolve drizzle duplicate declaration error

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,5 @@
 import {drizzle} from "drizzle-orm/neon-http";
-import {drizzle} from "drizzle-orm/neon-http";
+
 import {neon} from "@neondatabase/serverless"
 import { ENV } from "./env.js";
 import *  as schema from "../db/schema.js"
